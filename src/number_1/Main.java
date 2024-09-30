64dffe122880a85f265930eaa667619231635f30
wlsrn99 <wlsrn331@gmail.com>
2024-09-30 15:48:59 +0900

    1번문제 풀이

---
+++ src/number_1/Main.java
@@ -0,0 +1,45 @@
+package number_1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+/**
+ * 문제 설명
+ * 정수를 저장한 배열, arr 에서 가장 작은 수를 제거한 배열을 리턴하는 함수, solution을
+ * 완성해주세요. 단, 리턴하려는 배열이 빈 배열인 경우엔 배열에 -1을 채워 리턴하세요.
+ * 예를들어 arr이 [4,3,2,1]인 경우는 [4,3,2]를 리턴 하고, [10]면 [-1]을 리턴 합니다
+ *
+ * 제한 조건
+ * ● arr은 길이 1 이상인 배열입니다. -> 길이가 1일때 -1반환
+ * ● 인덱스 i, j에 대해 i ≠ j이면 arr[i] ≠ arr[j] 입니다. -> 중복된 수는 없다
+ */
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int[] arr = Arrays.stream(br.readLine().split(" "))
+			.mapToInt(Integer::parseInt).toArray();
+
+		int[] result = solution(arr);
+		System.out.println(Arrays.toString(result));
+	}
+
+	private static int[] solution(int[] arr) {
+		int len = arr.length;
+		if (len == 1) { //배열의 길이가 1일때
+			return new int[] {-1};
+		}else {
+			//최솟값 찾기
+			int min = Arrays.stream(arr).min().getAsInt();
+			int[] result = new int[len - 1];
+			int count = 0;
+			for (int current : arr) {
+				if (current != min) {
+					result[count++] = current;
+				}
+			}
+			return result;
+		}
+	}
+}
